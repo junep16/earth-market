f05c8c2b13336fb4188d27f05f0ac2a9b51fa8ce
junep16 <jiyunpark0816@gmail.com>
2022-01-20 11:28:05 +0900

    ✅ 네비게이션 및 이미지파일 추가

---
+++ earth-market/my-app/src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import "./navigation.css"; 
+
+export default function Navigation() {
+  return (
+    <div className="header"> 
+      <nav>
+        <ul>
+          <li>
+            <img src="../image/Logo-hodu.png" alt="호두 로고" />
+          </li>
+        </ul>
+      </nav> 
+    </div> 
+    );
+  }
---
+++ earth-market/my-app/src/Pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import Navigation from "../../Components/Navigation/Navigation";
+
+export default function Homepage() {   
+    return (
+    <div>
+        <Navigation></Navigation> 
+    </div>
+    );
+}
