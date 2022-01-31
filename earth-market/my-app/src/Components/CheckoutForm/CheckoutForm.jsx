c4adce00e30b305687d2ac828be7aa1b3d991d58
junep16 <jiyunpark0816@gmail.com>
2022-01-31 22:49:08 +0900

    🚧 폼 수정

--- earth-market/my-app/src/Components/CheckoutForm/CheckoutForm.jsx
+++ earth-market/my-app/src/Components/CheckoutForm/CheckoutForm.jsx
@@ -12,16 +12,40 @@ export default function CheckoutForm() {
             <label htmlFor="user-name">이름</label>
             <input type="text"></input>
           </div>
-          <div>
+          <PhoneNumber>
             <label htmlFor="user-phone">휴대폰</label>
             <input type="number" id="user-phone"></input>
             <input type="number" id="user-phone"></input>
             <input type="number" id="user-phone"></input>
-          </div>
+          </PhoneNumber>
           <div>
             <label htmlFor="user-email">이메일</label>
-            <input type="number" id="user-email"></input>
+            <input type="text" id="user-email"></input>
           </div>
+        </UserInfo>
+        <UserInfo>
+          <p>배송지 정보</p>
+          <div>
+            <label htmlFor="user-receiver">수령인</label>
+            <input type="text" id="user-receiver"></input>
+          </div>
+          <PhoneNumber>
+            <label htmlFor="user-phone">휴대폰</label>
+            <input type="number" id="user-phone"></input>
+            <input type="number" id="user-phone"></input>
+            <input type="number" id="user-phone"></input>
+          </PhoneNumber>
+          <div>
+            <label htmlFor="user-addresss">배송주소</label>
+            <input type="text" id="user-addresss"></input>
+            <button type="button">우편번호 조회</button>
+            <input type="text" id="user-addresss"></input>
+            <input type="text" id="user-addresss"></input>
+          </div> 
+          <div>
+            <label htmlFor="user-message">배송메세지</label>
+            <input type="text" id="user-message"></input> 
+          </div> 
         </UserInfo>
       </form>
     </div>
@@ -43,7 +67,7 @@ const UserInfo = styled.section`
   margin-top: 20px; 
   p {
     text-align: left; 
-    border-bottom: 1px solid #C4C4C4; 
+    border-bottom: 2px solid #C4C4C4; 
     width: 100%; 
     line-height: 2; 
   }
@@ -52,7 +76,7 @@ const UserInfo = styled.section`
     display: flex; 
     align-items: center; 
     justify-content: flex-start;  
-    flex-basis: 40px; 
+    flex-basis: 60px; 
     border-bottom: 1px solid #C4C4C4; 
     width: 100%; 
 
@@ -61,8 +85,22 @@ const UserInfo = styled.section`
       text-align: left;
       width: 100px;  
     }
-    input { 
-
+    input {  
+      height: 40px; 
+      width: 335px; 
+      border-style: none; 
+      border: 1px solid #C4C4C4; 
+      box-sizing: border-box; 
+      padding: 5px; 
+      font-size: 18px;  
     }
+  }
+`
+
+const PhoneNumber = styled.div`
+  #user-phone {
+    width: 100px; 
+  #user-phone::after {
+    content: "-"; 
   }
 `
