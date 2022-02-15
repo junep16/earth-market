3a36034400e332db752f0c342a42715b5770eb22
junep16 <jiyunpark0816@gmail.com>
2022-02-15 22:06:02 +0900

    ✨ 상품 데이터 불러오기

--- earth-market/my-app/src/Store/product.js
+++ earth-market/my-app/src/Store/product.js
@@ -2,17 +2,18 @@ import React, { createContext } from "react";
 import axios from 'axios';  
 import { API_ENDPOINT } from 'Constants/api';
 
-const getProductData = () => {
-  axios.get(API_ENDPOINT + '/products')
-    .then((response) => {
-      console.log(response.data.results) 
-    }); 
-}
-
 
 export const ProductContext = createContext(); 
-
 export const ProductStore = (props) => {
+
+  axios.get(API_ENDPOINT + '/products')
+    .then((response) => {
+      const result = (response.data.results); 
+      console.log(result); 
+    }); 
+    // for(let i=0; i > result.length; i++) {
+
+    // }
   const products = {
     name : "book", 
     company: "제코베", 
